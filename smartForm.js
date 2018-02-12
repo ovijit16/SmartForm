969803d2d88e948ca57bb616c85abe1c298d2fbe
ovijit16 <ovijitsarker5@gmail.com>
2018-02-12 09:22:54 +0600

    smart Form

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,45 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+
+	name: '',
+	email: ''
+
+	html: {likes:{},dislikes:{}},
+	css: {likes:{},dislikes:{}},
+	js: {likes:{},dislikes:{}},
+	strength: {css: '', js: '', html: ''}
+	currentQuestion: '#welcome'
+
+
+
+};
+
+$('#start').click(function(){
+
+	console.log("start");
+	$('#welcome').hide();
+	$('#q1').show();
+});
+
+
+$('name').change(function(event){
+
+
+	console.log($('#name').val());
+});
+
+$('#q1next').click(function(){
+ if($('#name').val() && $('#email').val()){
+
+
+ userData.name = $('#name').val();
+ userData.name = $('#email').val();
+ $('#q2').show();
+ $('#q1').show();
+  }
+  else{
+  	alert('Please enter name & email');
+  }
+
+});
